b8688553852b62c38a9d73e0de00021bf400f010
SeoungminKwon <tjdalsrnjs10@gmail.com>
2024-01-04 03:44:21 +0900

    Feat: 선택미션2

--- src/main/java/com/ll/midium/global/initData/NotProd.java
+++ src/main/java/com/ll/midium/global/initData/NotProd.java
@@ -12,19 +12,19 @@ import org.springframework.context.annotation.Profile;
 @Profile("!prod")
 @Configuration
 public class NotProd {
-    @Bean
-    public ApplicationRunner initNotProd(UserService userService, PostService postService) {
-
-        return args -> {
-            for (int i = 0; i < 100; i++) {
-                SiteUser user = userService.create("user" + i, "user" + i + "@naver.com", "1234");
-                Post post = new Post(user.getUsername(), "제목" + i, "내용" + i);
-                post.setPublish(true);
-                if (i % 2 == 0) {
-                    post.setPaid(true);
-                }
-                postService.savePost(post);
-            }
-        };
-    }
+//    @Bean
+//    public ApplicationRunner initNotProd(UserService userService, PostService postService) {
+//
+//        return args -> {
+//            for (int i = 0; i < 100; i++) {
+//                SiteUser user = userService.create("user" + i, "user" + i + "@naver.com", "1234");
+//                Post post = new Post(user.getUsername(), "제목" + i, "내용" + i);
+//                post.setPublish(true);
+//                if (i % 2 == 0) {
+//                    post.setPaid(true);
+//                }
+//                postService.savePost(post);
+//            }
+//        };
+//    }
 }
